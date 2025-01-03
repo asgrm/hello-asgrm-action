b98cb3c49f9dd56e75deb8c8ed2aad812247369f
asgrm <62254204+asgrm@users.noreply.github.com>
2025-01-03 08:25:03 +0000

    initial setup

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # hello-asgrm-action
-hello world action
+
+This action prints "Hello asgrm" or "Hello" + the name of a person to greet to the log.
+
+## Inputs
+
+### `name`
+
+**Required** The name of the person to greet. Default `"asgrm"`.
+
+## Outputs
+
+### `greeting`
+
+Greeting message
+
+## Example usage
+
+```yaml
+uses: actions/hello-asgrm-action
+with:
+  name: 'Mona the Octocat'
+```
--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-const name = core.getInput('name');
-const outputValue = `Hello ${name}`;
-core.setOutput('greeting', outputValue);
+try {
+  const name = core.getInput('name');
+  const outputValue = `Hello ${name}`;
+  core.setOutput('greeting', outputValue);
+} catch (error) {
+  core.setFailed(error.message);
+}
